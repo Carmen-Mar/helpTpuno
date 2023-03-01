99ed634554bce40a655c48f6bb375f8c9ec8535e
Carmen-Mar <123602821+Carmen-Mar@users.noreply.github.com>
2023-03-01 11:49:58 -0300

    Add files via upload
    Esta es mi primer experiencia en programación realizada. Surge de apuntes y ejemplos realizados durante la cursada. El enfoque ha sido experimentar para familiarizarme con el lenguaje.

---
+++ FEnd Ticmas CV/js/curriculo.js
@@ -0,0 +1,8 @@
+document.getElementById("").onclick = function () {
+    console.log ("");
+    document.getElementById("").innerHTML = "Carmen Marceñuk";
+}
+
+document.getElementById ('').addEventListener(''), function () {
+    document.body.style.backgroundColor = '1,IE4';
+}
